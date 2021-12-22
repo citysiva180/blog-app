fc7a1c4ee8bfd50d00004ba45d3f31c3ac3bf31a
Siva Rajan <citysiva180@gmail.com>
2021-12-22 21:10:58 +0530

    updated login and create post pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
+import CreatePost from "./Pages/CreatePost";
+import Login from "./Pages/Login";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={Home}></Route>
+        <Route path="/createpost" element={Home}></Route>
+        <Route path="/login" element={Home}></Route>
       </Routes>
     </Router>
   );
